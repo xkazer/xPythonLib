c1abead85091851b257def2e0212a78537b0a78d
xkazer <xkazer@sina.com>
2016-09-09 09:49:06 +0800

    first commit

---
+++ xencode.py
@@ -0,0 +1,54 @@
+﻿# -*- coding: UTF-8 -*-
+
+import sys, getopt
+import binascii
+
+g_key="key"
+
+def getFileName(path):
+    try:
+        return path[path.rindex('\\')+1:]
+    except:
+        return path
+
+def encode(data):
+    global g_key
+    
+    strEncode=""
+    for c in data:
+        strEncode += binascii.b2a_hex(chr(ord(c)^ord(g_key[0:1])))
+        
+    return strEncode
+    
+def decode(data):
+    global g_key
+    
+    strDecode=""
+    strData=binascii.a2b_hex(data)
+    for c in strData:
+        strDecode += chr(ord(c)^ord(g_key[0:1]))
+        
+    return strDecode
+        
+def printUsage():
+    print "Useage: ", getFileName(sys.argv[0]), "[-h] [-f filename]"
+
+if __name__ == "__main__":
+    opts, args = getopt.getopt(sys.argv[1:], "he:d:k:")
+    iMode=0
+    strData="Encode"
+    for op, value in opts:
+        if op == "-h":
+            printUsage()
+        elif op == "-e":
+            strData=value
+        elif op == "-d":
+            iMode=1
+            strData=value
+        elif op == "-k":
+            g_key=value
+            
+    if 1==iMode:
+        print decode(strData)
+    else:
+        print encode(strData).upper()
---
+++ xmd5.py
@@ -0,0 +1,33 @@
+﻿# -*- coding: UTF-8 -*-
+
+import sys, getopt
+import hashlib
+
+
+def getFileName(path):
+    try:
+        return path[path.rindex('\\')+1:]
+    except:
+        return path
+        
+def calcMd5(path):
+    m = hashlib.md5()
+    hFile = open(path, 'rb')
+    m.update(hFile.read())
+    hFile.close()
+    
+    return m.hexdigest()
+
+def printUsage():
+    print "Useage: ", getFileName(sys.argv[0]), "[-h] [-f filename]"
+
+if __name__ == "__main__":
+    opts, args = getopt.getopt(sys.argv[1:], "hf:")
+    for op, value in opts:
+        if op == "-h":
+            printUsage()
+        elif op == "-f":
+            strMd5 = calcMd5(value)
+            print "File:", value
+            print "md5:", strMd5
+            print "MD5:", strMd5.upper() 
